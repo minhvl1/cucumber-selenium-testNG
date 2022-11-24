5e45667c57d49753dbdf309db922312ca7e42f03
minhvl1 <minhvu890yahoo@gmail.com>
2022-11-24 16:58:35 +0700

    add more another way API testing

--- src/test/java/Steps/API.java
+++ src/test/java/Steps/API.java
@@ -1,17 +1,19 @@
 package Steps;
 
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.asserts.SoftAssert;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class API {
@@ -45,4 +47,46 @@ public class API {
         softAssert.assertEquals(getResponse.getStatusCode(),"200");
     }
 
+
+    //dont need get value api
+    @Given("test")
+    public void test() {
+
+        String baseUri = "https://fakerestapi.azurewebsites.net/api/v1";
+
+        // Request Scope
+        RequestSpecification request = RestAssured.given();
+
+        // add baseUri and basePath into the request
+        request.baseUri(baseUri);
+        request.basePath("/Activities");
+
+        // Response scope
+        final String FIRST_TODO = "/1";
+        Response response = request.get(FIRST_TODO);
+        response.prettyPrint();
+        response.then().body("id", equalTo(1));
+        response.then().body("title", equalTo("Activity 1"));
+        response.then().body("completed", equalTo(false));
+    }
+
+
+    @Given("test post")
+    public void testPost() {
+        String baseUri = "https://reqres.in/api/users";
+
+        // Request Scope
+        RequestSpecification request = RestAssured.given();
+        request.baseUri(baseUri);
+
+        request.header(new Header("Content-type", "application/json; charset=UTF-8"));
+        String requestBody = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+        Response response = request.body(requestBody).post("/posts");
+        response.prettyPrint();
+        response.then().body("name", equalTo("morpheus"));
+        response.then().body("job", equalTo("leader"));
+    }
 }
--- src/test/resources/features/API.feature
+++ src/test/resources/features/API.feature
@@ -1,10 +1,16 @@
 @Feature4
 Feature: Demo API
 
-  @get
+  @getvalueAPI
   Scenario: API get method
     Given send get method with id="1"
     When show get response body
     Then id contains "1"
 
+#  @getMethodApi
+#  Scenario: test api
+#    Given test
 
+#  @postMethodApi
+#  Scenario: test post api
+#    Given test post
