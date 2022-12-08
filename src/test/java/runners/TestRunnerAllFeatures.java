cedb2fdaadc038791fe09434dcb775deeffdd40d
minhvl1 <minhvu890yahoo@gmail.com>
2022-12-08 09:27:43 +0700

    update yml

--- src/test/java/runners/TestRunnerAllFeatures.java
+++ src/test/java/runners/TestRunnerAllFeatures.java
@@ -23,7 +23,7 @@ import java.io.File;
                 "json:target/cucumber-reports/cucumber-reports.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
          monochrome = true
-//        ,tags = "not @Feature4"
+        ,tags = "not @shopee"
 )
 
 public class TestRunnerAllFeatures extends AbstractTestNGCucumberTests {
