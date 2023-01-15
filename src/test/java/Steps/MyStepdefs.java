af33d41d2df87ca69f8391da663b00a751b1ddfe
minhvl1 <minhvu890yahoo@gmail.com>
2023-01-15 19:28:59 +0700

    add log4j

--- src/test/java/Steps/MyStepdefs.java
+++ src/test/java/Steps/MyStepdefs.java
@@ -5,17 +5,26 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.testng.asserts.SoftAssert;
 import pageObject.GooglePageObject;
+import org.apache.log4j.Logger;
+
+
 
 public class MyStepdefs {
 
     WebDriver driver;
 
     private GooglePageObject googlePageObject;
+    private static final Logger logger = Logger.getLogger(MyStepdefs.class);
 
     public MyStepdefs() {
+        BasicConfigurator.configure();
+        String log4jConfPath = "log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
         this.driver = Hooks.openAndQuitBrowser();
         googlePageObject = new GooglePageObject(driver);
     }
@@ -23,11 +32,14 @@ public class MyStepdefs {
     @Given("open browser")
     public void openBrowser() {
         driver.get("https://www.google.com.vn/?hl=vi");
+        logger.info("===Open browser===");
     }
 
     @When("Search {string}")
     public void search(String arg0) {
+        logger.info("===Start===");
         googlePageObject.sendKeySearchTextbox(arg0);
+
     }
 
     @And("Click search button")
@@ -37,6 +49,7 @@ public class MyStepdefs {
 
     @Then("See result page")
     public void seeResultPage() {
+        logger.info("===See result page===");
         System.out.println(driver.getCurrentUrl());
         try {
             Thread.sleep(3000);
@@ -47,6 +60,7 @@ public class MyStepdefs {
 
     @And("URL contains {string}")
     public void urlContains(String arg0) {
+        logger.info("===verify result page===");
         String url =driver.getCurrentUrl();
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertTrue(url.contains(arg0));
